73023de59cceb0dca62dcc7a736f1218bd56e23e
MatiasCardoso4 <cardosomatias1994@gmail.com>
2025-03-08 18:00:40 -0300

    Add copy icon and improve color generation feedback

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ const colorContainer = document.querySelector(".color-container");
 const colorId = document.querySelector(".color-id");
 
 generateBtn.addEventListener("click", function () {
+  const copyIcon = document.createElement('img')
+  copyIcon.src = './assests/copy.png'
   const hexColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, "0")}`;
   colorContainer.style.backgroundColor = hexColor;
   colorId.style.color = hexColor;
   colorId.textContent = hexColor;
+  colorId.append(copyIcon)
 });
 
 colorId.addEventListener("click", () => {
   const copiedAlert = document.createElement("span");
   copiedAlert.classList.add("copy");
   copiedAlert.textContent = "Copied!";
-  setTimeout(() => copiedAlert.classList.remove("copy"), 1500);
+  setTimeout(() => copiedAlert.classList.remove("copy"), 1000);
   navigator.clipboard.writeText(colorId.textContent);
   colorId.append(copiedAlert);
 });
